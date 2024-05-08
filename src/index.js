eab82c2456482b8c8ac6b03fd209a49406b5facc
Harsh Kumar <vijayk8478@gmail.com>
2024-05-08 23:33:13 +0530

    Cookie-parser and cors configured

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import { app } from './app.js';
 
 dotenv.config({
     path : './env'
 })
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.on("error", (error)=>{
+        console.log("server failed to listen on port", 8000);
+        throw error;
+    })
+    app.listen(8000,()=>{
+        console.log(`Server Started listening on port:`, 8000);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB Coonection Failed......",err);
+});
